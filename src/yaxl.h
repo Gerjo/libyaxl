bd479f4f48032e75aaee82fd242ad6bc1c152e7c
Gerjo <gerjoo@gmail.com>
2012-09-21 21:31:59 +0200

    A tidy single include header.

---
+++ src/yaxl.h
@@ -0,0 +1,15 @@
+/*
+ * File:   newfile.h
+ * Author: gerjo
+ *
+ * Created on September 21, 2012, 8:05 PM
+ */
+
+#ifndef YAXL_H
+#define	YAXL_H
+
+#include "sockets/Sockets.h"
+#include "file/File.h"
+
+#endif	/* YAXL_H */
+
