a068e2b40f9c014c558e15cdeb9cf82bb03a4769
Gerjo <gerjoo@gmail.com>
2012-10-16 17:08:09 +0200

    Project to compile as shared object on linux.

--- nbproject/Package-Debug.bash
+++ nbproject/Package-Debug.bash
@@ -12,9 +12,9 @@ CND_DISTDIR=dist
 CND_BUILDDIR=build
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libyaxl
-OUTPUT_BASENAME=libyaxl
-PACKAGE_TOP_DIR=libyaxl/
+OUTPUT_PATH=../dist/libyaxl.${CND_DLIB_EXT}
+OUTPUT_BASENAME=libyaxl.${CND_DLIB_EXT}
+PACKAGE_TOP_DIR=liblibyaxl.so/
 
 # Functions
 function checkReturnCode
@@ -59,15 +59,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/libyaxl/bin"
-copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
+makeDirectory "${NBTMPDIR}/liblibyaxl.so/lib"
+copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}lib/${OUTPUT_BASENAME}" 0644
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libyaxl.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/liblibyaxl.so.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libyaxl.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/liblibyaxl.so.tar *
 checkReturnCode
 
 # Cleanup
